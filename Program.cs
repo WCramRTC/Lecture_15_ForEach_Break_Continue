836621bb1be9b186a7310a401952dffc6af32099
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-21 18:03:40 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_15_ForEach_Break_Continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        } // Main
+
+        // ForEach
+        // What is the syntax
+        // What word replaces needing the index to access an element
+        // What can't you do with a foreach, that you can with a for loop
+
+        // Continue
+        // What does it do
+
+        // Break
+        // What does it do
+
+    } // class
+
+} // namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lecture_15_ForEach_Break_Continue
+ Programming 120 - Lecture 15 - ForEach, Continue, Break
